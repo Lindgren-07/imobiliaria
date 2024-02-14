09fb65ef2153f6db6e07116cf4099ed9fc7ce6df
Seu Nome <seu@email.com>
2024-02-14 16:42:53 -0300

    atualizando

--- static/script.js
+++ static/script.js
@@ -1,9 +1,24 @@
-var mensagem = document.querySelector('.menssagem_flash_excluir_all');
+var mensagem = document.querySelector('.menssagem_flash_excluir_all')
 
 if (mensagem) {
-    mensagem.classList.add('show'); 
+    mensagem.classList.add('show')
 
     setTimeout(function() {
-        mensagem.classList.remove('show'); 
-    }, 4000);
+        mensagem.classList.remove('show')
+    }, 4000)
+}
+
+let cont = 1
+document.getElementById("radio1").checked = true
+
+setInterval( function(){
+    nextImg()
+}, 4000)
+
+function nextImg(){
+    cont++
+    if(cont > 4){
+        cont = 1
+    }
+    document.getElementById("radio"+cont).checked = true
 }
